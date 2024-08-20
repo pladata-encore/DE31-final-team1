7e6f1dfa6d68caf53fffca9889c2c43625866341
clyde0909 <hayou94@gmail.com>
2024-08-20 03:01:06 +0000

    📝 Feat: [#131-01] 기능별 초안 작성
    Rule 페이지 상호작용 가능한 Flow 작성

--- FE/src/pages/dashboard/rules.jsx
+++ FE/src/pages/dashboard/rules.jsx
@@ -11,34 +11,14 @@ import {
 } from "@material-tailwind/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 import { ruleTableData } from "@/data";
-import { ReactFlow } from "@xyflow/react"
+import Flow from "@/widgets/cards/flow";
 
 import "@xyflow/react/dist/style.css";
 
 export function Rules() {
-  const initialNodes = [
-    {
-      id: '1',
-      type: 'input',
-      data: { label: 'Input Node' },
-      position: { x: 0, y: 0 },
-    },
-    {
-      id: '2',
-      type: 'output',
-      data: { label: <div>Output Node</div> },
-      position: { x: 100, y: 100 },
-    }
-  ];
-  const initialEdges = [
-    { id: 'e1-2', source: '1', target: '2', animated: true },
-    { id: 'e2-3', source: '2', target: '3', animated: true },
-  ];
   
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow elements={initialNodes} style={{zIndex:"99"}} />
-    </div>
+    <Flow width="100%" height="50vw" />
   );
 }
 
