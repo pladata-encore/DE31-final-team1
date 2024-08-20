67441b0c1cbf135d3852302cf46ffd716ff09a6d
DongHyeon <sswsoul89s@gmail.com>
2024-08-20 16:11:57 +0000

    feature/be #107 로그인 API 기능 구현
    JwtInfo 테이블의  IssuedAt, ExpiryAt 값 한국 시간으로 삽입되도록 수정

--- BE/app/v1/users/authorization/login.py
+++ BE/app/v1/users/authorization/login.py
@@ -30,31 +30,35 @@ async def login():
 
         # email 존재하지 않을 경우
         if not result:
-            return jsonify({"error": "존재하지 않는 이메일입니다."}), 400
+            return jsonify({"error": "존재하지 않는 이메일입니다."}), 401
 
         # 패스워드 일치 여부 확인
         if not bcrypt.checkpw(encoded_pwd, result.UserPwd.encode("utf-8")):
-            return jsonify({"error": "패스워드가 일치하지 않습니다."}), 400
+            return jsonify({"error": "패스워드가 일치하지 않습니다."}), 402
         
         # UserID를 통한 토큰 정보 불러오기
         search_token = await session.execute(select(JwtInfo).where(JwtInfo.UserID == result.UserID))
         result_token = search_token.scalar()
 
+        # 현재 시간 정의
+        issued_time = datetime.now() + timedelta(hours=9)
 
         # 토큰이 없을 때 생성하기
         if not result_token:
+            
+            expiry_time = issued_time + timedelta(hours=1)
 
             payload = {
                 "email" : email,
-                "issuedAt" : datetime.now().isoformat(),
-                "expiryAt" : (datetime.now() + timedelta(minutes=1)).isoformat()
+                "issuedAt" : issued_time.isoformat(),
+                "expiryAt" : expiry_time.isoformat()
             }
 
             # 토큰 생성
             token = jwt.encode(payload, current_app.config["JWT_SECRET_KEY"], "HS256")
 
             # 생성 토큰한 토큰 DB insert
-            new_token = JwtInfo(UserID=result.UserID, AccessToken=token, IssuedAt=datetime.now(), ExpiryAt=datetime.now() + timedelta(hours=1))
+            new_token = JwtInfo(UserID=result.UserID, AccessToken=token, IssuedAt=issued_time, ExpiryAt=expiry_time)
             session.add(new_token)
             await session.commit()
             
@@ -66,12 +70,14 @@ async def login():
 
 
         # 잔여시간이 30분 이하일 경우 토큰 만료시간을 30분으로 갱신
-        if result_token.ExpiryAt >= datetime.now() and result_token.ExpiryAt - datetime.now() <= timedelta(minutes=30):
+        if result_token.ExpiryAt >= issued_time and result_token.ExpiryAt - issued_time <= timedelta(minutes=30):
+
+            expiry_time = issued_time + timedelta(hours=30)
 
             payload = {
                 "email" : email,
-                "issuedAt" : datetime.now().isoformat(),
-                "expiryAt" : (datetime.now() + timedelta(minutes=30)).isoformat()
+                "issuedAt" : issued_time.isoformat(),
+                "expiryAt" : expiry_time.isoformat()
             }
 
             # 재발급 토큰 생성
@@ -83,8 +89,8 @@ async def login():
 
             if update_token:
                 update_token.AccessToken = token
-                update_token.IssuedAt = datetime.now()
-                update_token.ExpiryAt = datetime.now() + timedelta(minutes=30)
+                update_token.IssuedAt = issued_time
+                update_token.ExpiryAt = expiry_time
                 await session.commit()
 
                 return  jsonify({
@@ -94,8 +100,8 @@ async def login():
                     })
             
         # 토큰 시간이 만료되었을 경우             
-        if result_token.ExpiryAt < datetime.now():
-            return jsonify({"error": "토큰 시간이 만료되었습니다. 다시 로그인 해주세요."}), 444
+        if result_token.ExpiryAt < issued_time:
+            return jsonify({"error": "토큰 시간이 만료되었습니다. 다시 로그인 해주세요."}), 403
             
         # JwtInfo Table에 생성되어 있는 토큰 정보 반환
         return  jsonify({
