ed397286e35205bb9273a75921f2292c47bc188c
조태식 <whxotlr2@naver.com>
2024-09-04 16:49:09 +0900

    Feat [#146] - nifi-web
    프로세서,관계 삭제 수정 추가

--- BE/app/v1/nifi/nifi_api.py
+++ BE/app/v1/nifi/nifi_api.py
@@ -7,9 +7,11 @@ import httpx
 from app.v1.users.components.common import *
 import aiohttp
 from app.v1.nifi.components.nifi_default import *
+
+
 nifi_api_bp = Blueprint('nifi_api', __name__)
 load_dotenv()
-
+Mongo = os.getenv("Mongo")
 NIFI_URL = os.getenv("NIFI_URL") #https://192.168.1.234:8443/nifi-api/ 
 Mongo_URI = os.getenv("Mongo_URI")
 Mongo_Database_Name = os.getenv("Mongo_Database_Name")
@@ -45,7 +47,7 @@ async def create_consumekafka_processor():
     },
     "config": {
       "properties": {
-        "bootstrap.servers": "140.238.153.4:10000",
+        "bootstrap.servers": Mongo,
         "topic": "user1_device5",
         "group.id": "test_group2",
         "auto.offset.reset": "earliest",
@@ -54,7 +56,7 @@ async def create_consumekafka_processor():
       },
       "autoTerminatedRelationships": ["failure", "success"]
     }
-  }
+  } 
     }
     
     try:
@@ -68,6 +70,63 @@ async def create_consumekafka_processor():
     except Exception as e:
         print(f"An error occurred while creating the processor: {e}")
         return jsonify({"error": "An error occurred while creating the processor", "details": str(e)}), 500
+
+
+# consumkafka 수정 http://localhost:19020/v1/nifi_api/update_consumekafka/
+# @nifi_api_bp.route('/update_consumekafka/', methods=['GET', 'OPTIONS'])
+async def update_consumekafka_processor(version,position=None, topic=None, groupId=None, offset=None):
+    # token, user_process_group_id
+    token = await get_token()
+    # web 상에서 클릭하면 id정보과 우측에 뜨므로
+    processor_id = "baa559ac-0191-1000-0d61-b9fcba30dde2"
+    url = f"{NIFI_URL}processors/{processor_id}"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    # version 받기
+    body = {
+        "revision": {
+            "version": version
+        },
+        "component": {
+            "id" : processor_id,
+            "type": "org.apache.nifi.processors.kafka.pubsub.ConsumeKafka_2_6",
+            "bundle": {
+            "group": "org.apache.nifi",
+            "artifact": "nifi-kafka-2-6-nar",
+            "version": "1.20.0"
+        },
+        "name": "ConsumeKafka_2_6",
+        "position": position,
+        "config": {
+            "properties": {
+                "bootstrap.servers": Mongo,
+                "topic": topic,
+                "group.id": groupId,
+                "auto.offset.reset": offset,
+                "key.deserializer": "org.apache.kafka.common.serialization.StringDeserializer",
+                "value.deserializer": "org.apache.kafka.common.serialization.StringDeserializer"
+                },
+            "autoTerminatedRelationships": ["failure"]
+            }
+        } 
+    }
+    
+    try:
+        async with httpx.AsyncClient(verify=False) as client:
+            response = await client.put(url, headers=headers, json=body)
+            response_text = response.text
+            if response.status_code == 200:  # 성공적으로 생성되었을 경우
+                return jsonify({"message": "Processor created successfully", "response": response_text}), 200
+            else:
+                return jsonify({"error": "Failed to create processor", "details": response_text}), response.status_code
+    except Exception as e:
+        print(f"An error occurred while creating the processor: {e}")
+        return jsonify({"error": "An error occurred while creating the processor", "details": str(e)}), 500
+    # return jsonify(200)
+
 
 # putmongo 생성하기 http://localhost:19020/v1/nifi_api/make_putmongo/
 @nifi_api_bp.route('/make_putmongo/', methods=['GET', 'OPTIONS'])
@@ -140,19 +199,103 @@ async def create_putmongo_processor():
             response = await client.post(url, headers=headers, json=body)
             response_text = response.text
             if response.status_code == 200:  # 성공적으로 생성되었을 경우
-                return jsonify({"message": "Processor created successfully", "response": response_text}), 200
+                return jsonify({"message": "Processor updated successfully", "response": response_text}), 200
             else:
-                return jsonify({"error": "Failed to create processor", "details": response_text}), response.status_code
+                return jsonify({"error": "Failed to update processor", "details": response_text}), response.status_code
     except Exception as e:
-        print(f"An error occurred while creating the processor: {e}")
-        return jsonify({"error": "An error occurred while creating the processor", "details": str(e)}), 500
+        print(f"An error occurred while updating the processor: {e}")
+        return jsonify({"error": "An error occurred while updating the processor", "details": str(e)}), 500
+
+
+# putmongo 수정 http://localhost:19020/v1/nifi_api/update_putmongo/
+# @nifi_api_bp.route('/update_putmongo/', methods=['GET', 'OPTIONS'])
+async def update_putmongo_processor(version,position=None, Mongo_Database_Name_input=None, Mongo_Collection_Name_input=None):
+    # token, user_process_group_id
+    token = await get_token()
+    # web 상에서 클릭하면 id정보과 우측에 뜨므로
+    processor_id = "baa54b56-0191-1000-2a30-c80ba7354bd3"
+    url = f"{NIFI_URL}processors/{processor_id}"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    # version 받기
+    body = {
+        "revision": {
+            "version": version
+        },
+        "component": {
+            "id" : processor_id,
+            "type": "org.apache.nifi.processors.mongodb.PutMongo",
+            "name": "PutMongo",
+            "position": position,
+            "config": {
+                "properties": {
+                    "Mongo URI": Mongo_URI, 
+                    "Mongo Database Name": Mongo_Database_Name_input, # 이름
+                    "Mongo Collection Name": Mongo_Collection_Name_input, # 컬렉션
+                    "ssl-client-auth": "REQUIRED",
+                    "Mode": "insert",
+                    "Upsert": "false",
+                    "Update Query Key": None,
+                    "putmongo-update-query": None,
+                    "put-mongo-update-mode": "doc",
+                    "Write Concern": "ACKNOWLEDGED",
+                    "Character Set": "UTF-8"
+                },
+                "autoTerminatedRelationships": ["failure", "success"],
+                "schedulingPeriod": "0 sec",
+                "penaltyDuration": "30 sec",
+                "yieldDuration": "1 sec",
+                "executionNode": "ALL",
+                "bulletinLevel": "WARN",
+                "runDurationMillis": 0,
+                "concurrentlySchedulableTaskCount": 1,
+                "comments": "",
+                "lossTolerant": False,
+                "defaultConcurrentTasks": {
+                    "TIMER_DRIVEN": "1",
+                    "EVENT_DRIVEN": "0",
+                    "CRON_DRIVEN": "1"
+                },
+                "defaultSchedulingPeriod": {
+                    "TIMER_DRIVEN": "0 sec",
+                    "CRON_DRIVEN": "* * * * * ?"
+                },
+                "retryCount": 10,
+                "retriedRelationships": [],
+                "backoffMechanism": "PENALIZE_FLOWFILE",
+                "maxBackoffPeriod": "10 mins"
+            }
+        }
+    }
+    
+    try:
+        async with httpx.AsyncClient(verify=False) as client:
+            response = await client.put(url, headers=headers, json=body)
+            response_text = response.text
+            if response.status_code == 200:  # 성공적으로 생성되었을 경우
+                return jsonify({"message": "Processor updated successfully", "response": response_text}), 200
+            else:
+                return jsonify({"error": "Failed to update processor", "details": response_text}), response.status_code
+    except Exception as e:
+        print(f"An error occurred while updating the processor: {e}")
+        return jsonify({"error": "An error occurred while updating the processor", "details": str(e)}), 500
+    # return jsonify(200)
+
+
+
+
+
+
 
 
 # 프로세서그룹안에있는 프로세서 목록 가져오기 http://localhost:19020/v1/nifi_api/get_process_list/
 @nifi_api_bp.route('/get_process_list/', methods=['GET', 'OPTIONS'])
-async def get_processors_list():
+async def get_processors_list(UserId):
     token = await get_token()
-    user_process_group_id = await get_user_process_group_id(152)
+    user_process_group_id = await get_user_process_group_id(UserId)
     url = f"{NIFI_URL}process-groups/{user_process_group_id}/processors"
     headers = {
         "Authorization": f"Bearer {token}"
@@ -170,11 +313,15 @@ async def get_processors_list():
                     "Name": component['name'],
                     "Type": component['type'],
                     "Properties": component['config']['properties'],
-                    "Version": processor['revision']['version']
+                    "Version": processor['revision']['version'],
+                    "Position": {
+                        "x": component['position']['x'],
+                        "y": component['position']['y']
+                    }
                 }
                 parsed_data.append(processor_info)
             # print(parsed_data)
-            return jsonify(parsed_data)
+            return parsed_data
             # return jsonify(response.json())
 
     except Exception as e:
@@ -286,4 +433,40 @@ async def set_processor_run_status():
 
 
 
-# kafka streamingf
+# 프로세서 관계 전체 들고오기 http://localhost:19020/v1/nifi_api/get_all_connection/
+@nifi_api_bp.route('/get_all_connection/', methods=['GET', 'OPTIONS'])
+async def get_all_connection(UserId):
+    token = await get_token()
+    user_process_group_id = await get_user_process_group_id(UserId)
+    url = f"{NIFI_URL}process-groups/{user_process_group_id}/connections"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        async with httpx.AsyncClient(verify=False) as client:
+            response = await client.get(url, headers=headers)
+            connections = response.json().get('connections', [])
+            
+            parsed_data = []
+            for connection in connections:
+                connection_info = {
+                    "Connection ID": connection['component']['id'],
+                    "Source Processor ID": connection['component']['source']['id'],
+                    "Source Processor Name": connection['component']['source']['name'],
+                    "Destination Processor ID": connection['component']['destination']['id'],
+                    "Destination Processor Name": connection['component']['destination']['name'],
+                    "Version": connection['revision']['version']
+                }
+                parsed_data.append(connection_info)
+
+            # return parsed_data
+            return parsed_data
+
+    except Exception as e:
+        return jsonify({"error": "Failed to get processor list", "details": str(e)}), 500
+
+
+
+
+# kafka streaming
--- BE/app/v1/users/main.py
+++ BE/app/v1/users/main.py
@@ -1,6 +1,8 @@
 from quart import Blueprint
 from .components.common import *
 from .components.functions import *
+from app.v1.nifi.nifi_api import *
+from app.v1.nifi.components.nifi_default import *
 
 main_bp = Blueprint('main',__name__)
 
@@ -48,4 +50,166 @@ async def login():
         
         return login_token, status_code
         
+@main_bp.route('/rule_set_default/',methods=['GET','OPTIONS'])
+async def rule_set_default():
+    # test용으로 152번째 userid -> pgid -> processor id -> 추가적으로 connection까지
 
+    # 152번째 user의 pgid 가져오기
+    UserId = 152
+    user_process_group_id = await get_user_process_group_id(UserId)
+    print(user_process_group_id)
+    # processor_list에 관한 정보 받기 , id, name, properties, type, version만
+    user_processor_list = await get_processors_list(UserId)
+    # ids = [processor['ID'] for processor in user_processor_list]
+
+    # connection 목록
+    user_connection_list = await get_all_connection(UserId)
+
+    
+    result_dict = {
+        "processors": user_processor_list,
+        "connections": user_connection_list
+    }
+
+    # print(ids)
+    return jsonify(result_dict)
+
+# consemkafka 수정하기
+@main_bp.route('/update_consumekafka/', methods=['GET', 'OPTIONS'])
+async def update_consumekafka():
+    UserId = 152
+    user_processor_list = await get_processors_list(UserId)
+    for processor in user_processor_list:
+        if processor['Name'] == 'ConsumeKafka_2_6':
+            version = processor['Version']
+            break
+    print(version)
+    # postion의 x,y 좌표는 받아올수있으므로
+    position = {
+      "x": 600,
+      "y": 500
+    }
+    topic = "user1_device5"
+    groupId = "user1_device5"
+    offset = "latest"
+    response = await update_consumekafka_processor(version,position=position, topic=None, groupId=None, offset=None)
+    response = await rule_set_default()
+    return response
+
+# putmongo 수정하기
+@main_bp.route('/update_putmongo/', methods=['GET', 'OPTIONS'])
+async def update_putmongo():
+    UserId = 152
+    user_processor_list = await get_processors_list(UserId)
+    for processor in user_processor_list:
+        if processor['Name'] == 'PutMongo':
+            version = processor['Version']
+            break
+    print(version)
+    # postion의 x,y 좌표는 받아올수있으므로
+    position = {
+      "x": 800,
+      "y": 500
+    }
+    # 기존에 존재하는 Database와 Collection_Name이어야한다.
+    Mongo_Database_Name = "testdb"
+    Mongo_Collection_Name = "TEST_NIFI3"
+    response = await update_putmongo_processor(version,position=position, Mongo_Database_Name_input=None, Mongo_Collection_Name_input =None)
+    response = await rule_set_default()
+    return response
+
+# processor 삭제하기
+@main_bp.route('/delete_processors/', methods=['GET', 'OPTIONS'])
+async def delete_processors():
+    UserId = 152
+    token = await get_token()
+    # 이미 processor_id를 앍고있으므로 
+    
+    consumkafka_id = "bbf68884-0191-1000-b740-7743289a0d42"
+    putmongo_id = "baa54b56-0191-1000-2a30-c80ba7354bd3"
+    user_processor_list = await get_processors_list(UserId)
+    # print(user_processor_list)
+    for processor in user_processor_list:
+        if processor['ID'] == putmongo_id:
+            version = processor['Version']
+            break
+    # print(version)
+    client_id = await get_client_id()
+    # print(client_id)
+    url = f"{NIFI_URL}processors/{putmongo_id}"
+    params = {
+        'version': version,
+        'clientId': client_id
+    }
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+
+    # 요청 보내기 전에 관계를 일단 지우자
+    user_connection_list = await get_all_connection(UserId)
+    # print(user_connection_list)
+    # 관계는 누군가의 출발점이자 누군가의 도착점이으로 2개조회
+    processor_connection_info = [
+    {
+        "Connection ID": connection['Connection ID'],
+        "Version": connection['Version']
+    }
+    for connection in user_connection_list
+    if connection['Destination Processor ID'] == putmongo_id or connection['Source Processor ID'] == putmongo_id
+    ]
+
+
+    # print(processor_connection_info)
+    # 이제 관계를 끊기
+    
+    for i in range(len(processor_connection_info)):
+        connection_id = processor_connection_info[i]["Connection ID"]
+        version = processor_connection_info[i]["Version"]
+        await delete_connections(connection_id, version)
+
+
+    try:
+        async with httpx.AsyncClient(verify=False) as client:
+            response = await client.delete(url, headers=headers, params=params)
+            response_text = response.text
+            if response.status_code == 200:  # 성공적으로 삭제되었을 경우
+                # return jsonify({"message": "Processor deleted successfully", "response": response_text}), 200
+                response = await rule_set_default()
+                return response
+            else:
+                return jsonify({"error": "Failed to delete processor", "details": response_text}), response.status_code
+    except Exception as e:
+        print(f"An error occurred while deleting the processor: {e}")
+        return jsonify({"error": "An error occurred while deleting the processor", "details": str(e)}), 500
+    # return jsonify(200)
+
+
+
+# connection 삭제하기
+@main_bp.route('/delete_connections/', methods=['GET', 'OPTIONS'])
+async def delete_connections(connection_id,version):
+    UserId = 152
+    token = await get_token()
+    # version, token, client_id, conneciton_id
+    client_id = await get_client_id()
+    # print(client_id)
+    url = f"{NIFI_URL}connections/{connection_id}"
+    params = {
+        'version': version,
+        'clientId': client_id
+    }
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    try:
+        async with httpx.AsyncClient(verify=False) as client:
+            response = await client.delete(url, headers=headers, params=params)
+            response_text = response.text
+            if response.status_code == 200:  # 성공적으로 삭제되었을 경우
+                return jsonify({"message": "Processor deleted successfully", "response": response_text}), 200
+            else:
+                return jsonify({"error": "Failed to delete processor", "details": response_text}), response.status_code
+    except Exception as e:
+        print(f"An error occurred while deleting the processor: {e}")
+        return jsonify({"error": "An error occurred while deleting the processor", "details": str(e)}), 500
+    # return jsonify(200)
